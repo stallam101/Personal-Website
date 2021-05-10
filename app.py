765d93799be7bc8efe4bc3d3c99f386a5fd93f29
Sahil Tallam <sahiltallam@kanwarsair6608.lan>
2021-05-09 21:07:28 -0700

    Conv2D

--- app.py
+++ app.py
@@ -27,7 +27,9 @@ def train():
         data = data / 255
 
         model = keras.Sequential([
-            keras.layers.Flatten(input_shape=(100, 100, 3)),
+            keras.layers.Conv2D(filters=32, kernel_size=(3,3), input_shape=(100,100,3)),
+            keras.layers.Maxpooling2D(pool_size=(2,2)),
+            keras.layers.Flatten(),
             keras.layers.Dense(256, activation='relu'),
             keras.layers.Dense(2, activation='softmax')
         ])
